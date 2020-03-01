fb74dfa07cc80648d714d9746bba42146eafd968
kennedy0597 <kennedy0597@gmail.com>
2020-03-01 16:47:02 +0800

    added proper loop and tries

--- Bash/hangman.sh
+++ Bash/hangman.sh
@@ -4,24 +4,30 @@
 echo "Welcome to hangman"
 echo "You have 5 tries"
 
-message(){
+welcome(){
     echo "enter your alphabet guess"
     echo "you have $try left"
 }
 
+function guessing(){
+while [ $try != 0 ]; do
 read guess1
-
+  if [[ $word == *$guess1* ]]; then
+    echo "correct guess"
+    echo $guess | sed s/./$guess1/5
+    continue 
+  else
+    echo wrong
+    echo "$(($try - 1)) tries left" 
+    try="$(($try - 1))"
+    continue
+  fi
+done
+echo "no mo tries"
+}
 try=5
 word=python
 guess=------
-if $try != 0; then
-  if [[ $word == *$guess1* ]]; then
-    echo "correct guess"
-    echo $guess | sed -r 's/[-]+/'$guess1'/g' 
-  else 
-    echo wrong
-    echo "$(($try - 1)) tries left" 
-  fi
-else
-  echo "no more tries"
-fi
+
+welcome
+guessing
