14cc85b82156b9557f80dac247650aa91d4ef466
kennedy0597 <kennedy0597@gmail.com>
2020-03-01 17:05:39 +0800

    finish version 1

--- Bash/hangman.sh
+++ Bash/hangman.sh
@@ -12,42 +12,45 @@ welcome(){
 function guessing(){
 while [ $try != 0 ]; do
 read guess1
-  if [[ "p" == *$guess1* ]]; then
+  if [[ "p" == $guess1 ]]; then
     echo "correct guess"
     echo $guess | sed s/./$guess1/1
     guess2="$(echo $guess | sed s/./$guess1/1)"
     guess=$guess2
     continue 
-  elif [[ "y" == *$guess1* ]]; then
+  elif [[ "y" == $guess1 ]]; then
     echo "correct guess"
     echo $guess | sed s/./$guess1/2
     guess3="$(echo $guess | sed s/./$guess1/2)"
     guess=$guess3
     continue
-  elif [[ "t" == *$guess1* ]]; then
+  elif [[ "t" == $guess1 ]]; then
     echo "correct guess"
     echo $guess | sed s/./$guess1/3
     guess4="$(echo $guess | sed s/./$guess1/3)"
     guess=$guess4
     continue
-  elif [[ "h" == *$guess1* ]]; then
+  elif [[ "h" == $guess1 ]]; then
     echo "correct guess"
     echo $guess | sed s/./$guess1/4
     guess5="$(echo $guess | sed s/./$guess1/4)"
     guess=$guess5
     continue
-  elif [[ "o" == *$guess1* ]]; then
+  elif [[ "o" == $guess1 ]]; then
     echo "correct guess"
     echo $guess | sed s/./$guess1/5
     guess6="$(echo $guess | sed s/./$guess1/5)"
     guess=$guess6
     continue
-  elif [[ "n" == *$guess1* ]]; then
+  elif [[ "n" == $guess1 ]]; then
     echo "correct guess"
     echo $guess | sed s/./$guess1/6
     guess7="$(echo $guess | sed s/./$guess1/6)"
     guess=$guess7
     continue
+  elif [[ $guess = $word ]]; then
+    echo "Congratulatios, you guessed correctly with $try tries left"
+    break 
   else
     echo wrong
     echo "$(($try - 1)) tries left" 
@@ -55,7 +58,6 @@ read guess1
     continue
   fi
 done
-echo "no mo tries"
 }
 try=5
 word=python
